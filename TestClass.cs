409e2736609d50a80445260e80a63afd9a0351b8
Shamail Ahmad <zeistsam@gmail.com>
2023-10-21 16:39:08 +0530

    Feature2022| Added Stash TestClass

--- TestClass.cs
+++ TestClass.cs
@@ -12,4 +12,9 @@ public class Class1
     {
         Console.WriteLine("Adding test class from 2019);
     }
+
+    public Class2()
+    {
+        Console.WriteLine("This is a Test Class file from 2022-02");
+    }
 }
